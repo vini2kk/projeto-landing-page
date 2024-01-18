b62d1f871ef9a18729e037b87e3e6e8df3bc9f7b
vini2kk <viniciusdeoliveiramelo2@gmail.com>
2024-01-18 18:22:47 -0300

    versao Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaesquerda =window.document.getElementById("seta-esquerda")
+var setadireita = window.document.getElementById("seta-direita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+
+function rolarparadireita(){
+    leonardo.style ="display:none" 
+    bruna.style ="display:flex"
+    setadireita.style = "display:none"
+    setaesquerda.style ="display:flex; margin-top:55px"
+}
+
+function rolarparaesquerda(){
+    leonardo.style="display:flex"
+    bruna.style="display:none"
+    setadireita.style="display:flex; margin-top:55px"
+    setaesquerda.style="display:none"
+}
+
